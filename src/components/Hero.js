68f020d07b727c304ce07cf7664ff1226596a7ef
Jacob Van Schenck <jacob.vanschenck@gmail.com>
2022-06-21 13:45:11 -0500

    adds scroll smooth, updates nav

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,8 @@
 export default function Hero() {
   return (
-    <div className="flex h-screen w-screen p-48">
-      <div className="mx-auto self-center">
+    <div className="flex h-screen w-screen px-48">
+      <div className="mx-auto mt-32">
+        <h1 className="mb-20 text-center text-8xl">Jacob Van Schenck</h1>
         <p className="pb-20 text-3xl">
           Hi, my name is Jacob and I am a Frontend Developer! I enjoy solving
           complex problems by writing clean, well tested code that scales.
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,41 @@
 export default function Nav() {
   return (
-    <nav className="fixed z-50 flex w-full justify-around bg-green-500">
+    <nav className="fixed z-50 flex w-full justify-around bg-green-500 shadow-lg">
       <ul className="flex justify-evenly">
-        <li className="h-full w-full border-b-4 border-red-400 bg-red-400 py-4 px-6 transition duration-200 hover:border-solid hover:border-blue-400 hover:text-blue-400">
+        <li className="h-full w-full border-b-4 border-red-400 bg-red-400 py-3 px-6 transition duration-200 hover:border-solid hover:border-blue-400 hover:text-blue-400 ">
           <a href="/">Home</a>
         </li>
-        <li className="h-full w-full border-b-4 border-red-400 bg-red-400 py-4 px-6 transition duration-200 hover:border-solid hover:border-blue-400 hover:text-blue-400">
-          <a href="/#projects">Projects</a>
+        <li className="h-full w-full border-b-4 border-red-400 bg-red-400 py-3 px-6 transition duration-200 hover:border-solid hover:border-blue-400 hover:text-blue-400">
+          <a
+            className=" focus:border-blue-400 focus:text-blue-400"
+            href="#projects"
+          >
+            Projects
+          </a>
         </li>
-        <li className="h-full w-full border-b-4 border-red-400 bg-red-400 py-4 px-6 transition duration-200 hover:border-solid hover:border-blue-400 hover:text-blue-400">
-          <a href="/#about">About</a>
+        <li className="h-full w-full border-b-4 border-red-400 bg-red-400 py-3 px-6 transition duration-200 hover:border-solid hover:border-blue-400 hover:text-blue-400">
+          <a
+            className=" focus:border-blue-400 focus:text-blue-400"
+            href="#about"
+          >
+            About
+          </a>
         </li>
-        <li className="h-full w-full border-b-4 border-red-400 bg-red-400 py-4 px-6 transition duration-200 hover:border-solid hover:border-blue-400 hover:text-blue-400">
-          <a href="/#contact">Contact</a>
+        <li className="h-full w-full border-b-4 border-red-400 bg-red-400 py-3 px-6 transition duration-200 hover:border-solid hover:border-blue-400 hover:text-blue-400">
+          <a
+            className=" focus:border-blue-400 focus:text-blue-400"
+            href="#contact"
+          >
+            Contact
+          </a>
         </li>
-        <li className="h-full w-full border-b-4 border-red-400 bg-red-400 py-4 px-6 transition duration-200 hover:border-solid hover:border-blue-400 hover:text-blue-400">
-          <a href="/resume">Resume</a>
+        <li className="h-full w-full border-b-4 border-red-400 bg-red-400 py-3 px-6 transition duration-200 hover:border-solid hover:border-blue-400 hover:text-blue-400 ">
+          <a
+            className=" focus:border-blue-400 focus:text-blue-400"
+            href="/resume"
+          >
+            Resume
+          </a>
         </li>
       </ul>
     </nav>
