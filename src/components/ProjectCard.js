a4e73b61259366fc4cf40fa8864b078f62ae7c44
Jacob Van Schenck <jacob.vanschenck@gmail.com>
2022-06-29 21:38:49 -0500

    adds links to github/live site buttons, updates project images/animations

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,30 +1,50 @@
 import { AiFillGithub } from 'react-icons/ai'
 import { Link } from 'react-router-dom'
 
-export default function ProjectCard({ name, imgUrl, route }) {
+export default function ProjectCard({
+  name,
+  subtitle,
+  imgUrl,
+  route,
+  githubUrl,
+  siteUrl,
+}) {
   return (
-    <Link className="flex-col" to={`/${route}`}>
-      <div className="group relative rounded-full shadow-lg duration-300 hover:scale-105">
-        <h3 className="absolute top-1/2 w-full -translate-y-1/2 text-center text-2xl transition-opacity duration-200 group-hover:opacity-0">
-          {name}
-        </h3>
-        <div className="w-full self-center">
-          <img
-            className="rounded-full object-contain"
-            src={imgUrl}
-            alt={name}
-          />
+    <div className="flex-col">
+      <Link to={`/${route}`}>
+        <div className="group relative w-72 rounded-full shadow-lg duration-300 hover:scale-105">
+          <div className="absolute top-1/2 z-50 w-full -translate-y-1/2 text-center transition-opacity duration-300 group-hover:opacity-0">
+            <h3 className="text-3xl text-white">{name}</h3>
+            <p className="font-thin text-white">{subtitle}</p>
+          </div>
+          <div className="rounded-full bg-black">
+            <img
+              className="h-72 w-full rounded-full object-cover opacity-50 transition-opacity duration-300 hover:opacity-100"
+              src={imgUrl}
+              alt={name}
+            />
+          </div>
         </div>
-      </div>
+      </Link>
       <div className="flex justify-center space-x-4 pt-6 text-white">
-        <button className="flex items-center rounded-md bg-indigo-500 px-5 py-3 transition duration-300 hover:scale-105 hover:shadow-lg hover:shadow-indigo-500/50">
+        <a
+          href={githubUrl}
+          target="_blank"
+          className="flex items-center rounded-md bg-indigo-500 px-5 py-3 transition duration-300 hover:scale-105 hover:shadow-lg hover:shadow-indigo-500/50"
+          rel="noreferrer"
+        >
           <AiFillGithub className="mr-2 text-xl" />
           Github
-        </button>
-        <button className="rounded-md bg-teal-500 px-5 py-3 transition duration-300 hover:scale-105 hover:shadow-lg hover:shadow-teal-500/50">
+        </a>
+        <a
+          href={siteUrl}
+          target="_blank"
+          className="rounded-md bg-teal-500 px-5 py-3 transition duration-300 hover:scale-105 hover:shadow-lg hover:shadow-teal-500/50"
+          rel="noreferrer"
+        >
           Live Site
-        </button>
+        </a>
       </div>
-    </Link>
+    </div>
   )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,17 +7,30 @@ export default function Projects() {
   return (
     <div className="p-5" id="projects">
       <h2 className="text-4xl">Projects Section</h2>
-      <div className="flex flex-row space-x-10 px-32 py-10">
+      <div className="flex flex-col justify-center space-x-10 px-32 py-10 md:flex-row">
         <ProjectCard
           name={'Turbo Runners'}
+          subtitle={'NFT Project'}
           imgUrl={turboRunnersImg}
           route={'project1'}
+          githubUrl={'https://github.com/jacobvanschenck/nft-project-nextjs'}
+          siteUrl={'https://turbo-runners.vercel.app/'}
         />
-        <ProjectCard name={'MyDex'} imgUrl={dexImg} route={'project1'} />
+        <ProjectCard
+          name={'MyDex'}
+          subtitle={'Decentralized Exchange'}
+          imgUrl={dexImg}
+          route={'project1'}
+          githubUrl={'https://github.com/jacobvanschenck/My-Dex'}
+          siteUrl={'https://dex-vs.netlify.app/'}
+        />
         <ProjectCard
           name={'MultisigWallet'}
+          subtitle={'Multiple validater crypto wallet'}
           imgUrl={walletImg}
           route={'project1'}
+          githubUrl={'https://github.com/jacobvanschenck/Multisig-Wallet'}
+          siteUrl={'https://multi-sig-wallet-vs.netlify.app/'}
         />
       </div>
     </div>
