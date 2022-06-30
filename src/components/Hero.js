70bfedc92029ebf12728726defc943bd7fe86c57
Jacob Van Schenck <jacob.vanschenck@gmail.com>
2022-06-30 13:31:01 -0500

    updates hero CTA

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,21 @@
+import { BiChevronDown } from 'react-icons/bi'
+
 export default function Hero() {
   return (
-    <div className="flex h-screen w-screen px-48">
+    <div className="flex h-screen w-screen px-16">
       <div className="mx-auto self-center ">
         <h1 className="mb-20 text-center text-8xl">Jacob Van Schenck</h1>
         <p className="pb-20 text-3xl">
           Hi, my name is Jacob and I am a Frontend Developer! I enjoy solving
           complex problems by writing clean, well tested code that scales.
         </p>
-        <button className="rounded-md bg-teal-500 px-5 py-3 text-white transition-shadow duration-200 hover:shadow-lg hover:shadow-teal-500/50">
-          Let's Connect
-        </button>
+        <a
+          href="#projects"
+          className="flex w-52 items-center justify-center rounded-md bg-teal-500 px-5 py-3 text-white transition-shadow duration-200 hover:shadow-lg hover:shadow-teal-500/50"
+        >
+          Check out my work
+          <BiChevronDown className="ml-1 text-xl" />
+        </a>
       </div>
     </div>
   )
